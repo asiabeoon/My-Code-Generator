025db150ced566ed86288c43d6674a110f6d5b83
asiabeoon <a.hashbey@gmail.com>
2023-04-04 22:37:23 -0400

    added variables for user input

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,12 @@ var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar =["!", "#", "$", "%", "^", "&", "*", "<", ">", "/", "~", "_", "-", "+"]; 
 
+// Setting Variables for Prompts for User Input
 
+var hasNumberChar;
+var hasUpperCase;
+var hasLowerCase;
+var hasSpecialChar;
 
 
 // Get references to the #generate element
