1092609cb0c1c7957a8a715cb614850870a85bd8
asiabeoon <a.hashbey@gmail.com>
2023-04-04 22:16:01 -0400

    added character arrays for numbers, upper case and lower case, and special characters

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+// Setting Arrays characters that the generator can use
+var numberChar = ["0","1","2","3", "4", "5", "6", "7", "8", "9"];
+var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar =["!", "#", "$", "%", "^", "&", "*", "<", ">", "/", "~", "_", "-", "+"]; 
+
+
 
 
 // Get references to the #generate element
